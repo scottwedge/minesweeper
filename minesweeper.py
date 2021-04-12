3dbc1e6d807bf9d887b2eca86faa2062fdfbec05
Scott Wedge <scott.wedge@gmail.com>
2021-04-11 21:30:57 -0400

    Assign constants, Create grid and Show grid and Get level functions

--- minesweeper.py
+++ minesweeper.py
@@ -2,3 +2,52 @@
 
 # Simple MineSweeper program
 
+# Import
+
+# Constants
+X = 30  # width
+Y = 16  # height
+BEGINNER = 33 # number of mines based on difficulty level
+MEDIUM = 66
+EXPERT = 99
+UNKNOWN = "."
+MINE = "M"
+BLANK = " "
+
+
+
+def create_grid(height, width, init_value):
+    game_list = [] # initialize blank list
+    for j in range(height * width):
+        game_list.append(init_value)
+    return game_list
+
+def show_grid(x, y, grid):
+    for j in range(y):
+        row = ""  # initialize blank row
+        for k in range(x):
+            row = row + grid[k + j*x]  # add to row
+        print(row)
+           
+def get_level():  # determine what level of game to play
+    valid_value = False
+    while not valid_value:
+        level = input("What level of game do you want:\n 1. Beginner\n 2. Medium\n 3. Expert\n")
+        if level not in ["1", "2", "3"]:
+            print("{} is not a valid value ... try again!".format(level))
+        else:
+            valid_value = True
+    return level
+
+def add_mines(grid):
+    pass
+
+
+
+grid = create_grid(X, Y, UNKNOWN)
+
+show_grid(X, Y, grid)
+
+level = get_level()
+
+print(level)   # DEBUG
